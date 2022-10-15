5826ce5b5d008f33b474e56345322b746302231c
H0R15H0 <shohori13@gmail.com>
2022-10-15 11:25:53 +0900

    add sample code

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,9 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hello world");
+  Serial.println("こんにちは");
 
+  delay(5000);
 }
