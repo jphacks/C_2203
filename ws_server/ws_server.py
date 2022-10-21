0fdbab2cb2e90c95b008d760e622d0a7c43d3b2b
Hide-nu <nkret805worm@gmail.com>
2022-10-21 15:38:52 +0900

    add command

--- ws_server/ws_server.py
+++ ws_server/ws_server.py
@@ -7,15 +7,30 @@ app.config['SECRET_KEY'] = 'secret_key'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 # 命令を受け取るページ
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 # /animateにアクセスが来たら命令を送る
-@app.route('/animate')
-def animate():
-    socketio.emit('animate', "Animate event is requested.")
-    return "Animate event is requested."
+@app.route('/animate/walk')
+def walk():
+    socketio.emit('walk', "walk request")
+    return "walk event is requested."
+
+@app.route('/animate/stop')
+def stop():
+    socketio.emit('stop', "stop request")
+    return "stop event is requested."
+
+@app.route('/animate/catch')
+def catch():
+    socketio.emit('catch', "catch request")
+    return "catch event is requested."
+
+@app.route('/animate/release')
+def release():
+    socketio.emit('release', "release request")
+    return "release event is requested."
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
